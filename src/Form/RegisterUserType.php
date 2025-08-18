25c1fd5bcb8bd52c4ad4b98713919a8fa7055582
Harry EMARH <emarh.harry.code@gmail.com>
2025-08-18 20:19:47 +0200

    Submit

--- src/Form/RegisterUserType.php
+++ src/Form/RegisterUserType.php
@@ -4,6 +4,7 @@ namespace App\Form;
 
 use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -17,6 +18,7 @@ class RegisterUserType extends AbstractType
             ->add('password')
             ->add('firstName')
             ->add('lastName')
+            ->add('submit', SubmitType::class)
         ;
     }
 
