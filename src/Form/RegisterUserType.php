e7737f71bfd018c00e5d403ef57029165d3ed561
Harry EMARH <emarh.harry.code@gmail.com>
2025-08-18 23:44:19 +0200

    FirstName field

--- src/Form/RegisterUserType.php
+++ src/Form/RegisterUserType.php
@@ -7,6 +7,7 @@ use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -15,7 +16,12 @@ class RegisterUserType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('firstName')
+            ->add('firstName', TextType::class, [
+                'label' => 'Votre prénom',
+                'attr' => [
+                    'placeholder' => 'Indiquez votre prénom'
+                ]
+            ])
             ->add('lastName')
             ->add('email', EmailType::class, [
                 'label' => 'Votre adresse email',
