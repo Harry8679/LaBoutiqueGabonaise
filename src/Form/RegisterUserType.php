2f8867c75b5ada60f39c29989d6330f92d30fcc5
Harry EMARH <emarh.harry.code@gmail.com>
2025-08-18 23:39:12 +0200

    Password field

--- src/Form/RegisterUserType.php
+++ src/Form/RegisterUserType.php
@@ -5,6 +5,7 @@ namespace App\Form;
 use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
@@ -14,6 +15,8 @@ class RegisterUserType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
+            ->add('firstName')
+            ->add('lastName')
             ->add('email', EmailType::class, [
                 'label' => 'Votre adresse email',
                 'attr' => [
@@ -21,9 +24,12 @@ class RegisterUserType extends AbstractType
                 ]
             ])
             // ->add('roles')
-            ->add('password')
-            ->add('firstName')
-            ->add('lastName')
+            ->add('password', PasswordType::class, [
+                'label' => 'Votre mot de passe',
+                'attr' => [
+                    'placeholder' => 'Indiquez votre mot de passe'
+                ]
+            ])
             ->add('submit', SubmitType::class)
         ;
     }
